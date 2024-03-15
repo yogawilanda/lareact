a8341a5bc8459d41d7680a4643121d107fd2c35f
Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-03-15 14:34:12 +0700

    feat: Improve and refactor code on some pages

--- resources/js/Components/ShadowButtonPrimary.jsx
+++ resources/js/Components/ShadowButtonPrimary.jsx
@@ -4,9 +4,16 @@ import { Button, Link } from '@nextui-org/react'; // Import necessary components
 
 function ShadowButtonPrimary({ onPress, href, children }) {
     return (
-        <Button variant='shadow' color='primary' onPress={onPress ? onPress : () => console.log("Unidentified Button Action")}>
-            <Link href={href ? href : "/"} className='text-white dark:text-white'>
-                {children}
+        <Button
+            variant='shadow'
+            color='primary'
+            onPress={onPress ? onPress : () => console.log("Unidentified Button Action")}
+        >
+            <Link
+                href={href ? href : "/"}
+                className='text-white dark:text-white'
+            >
+                {children ? children : "Undefined Button"}
             </Link>
         </Button>
     );
